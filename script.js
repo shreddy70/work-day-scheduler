bcdd202dc0b2df1dc5b85b0290417d0ea83f3293
git <oneenzoo@Enzos-iMac.local>
2021-11-27 21:12:23 -0700

    third update

--- script.js
+++ script.js
@@ -11,3 +11,60 @@
 // THEN the text for that event is saved in local storage
 // WHEN I refresh the page
 // THEN the saved events persist
+
+// Display current date and time
+setInterval(function() {
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+} ,1000)
+
+// FOR EACH past, present, and future run loop function
+// Pass each id= hour8, hour9, etc through each loop function
+
+function timeLoop() {
+var timeNow = moment().hour();
+
+$(".time-block").each(function () {
+var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+if (blockTime < timeNow) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
+}
+else if (blockTime === timeNow) {
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).addClass("present");
+}
+else {
+    $(this).removeClass("present");
+    $(this).removeClass("past");
+    $(this).addClass("future");
+}
+    })
+}
+
+// saveBtn event listener
+$(document).ready(function () {
+         
+$(".saveBtn").on("click", function () {
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    
+// Save to local storage
+localStorage.setItem(time, text);
+})
+        
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
+    
+timeLoop();
+})
